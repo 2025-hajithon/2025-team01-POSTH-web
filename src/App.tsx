a277378f6c25459762551f988524153779876ad2
leejuhi <anim0203@naver.com>
2025-07-26 22:20:11 +0900

    feat: 답장 페이지

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import MainLayout from "@/layouts/main-layout";
 import HomePage from "@/pages/home-page";
-import QuestionPage from "@/pages/question-page";
-import QuestionSubmitPage from "@/pages/question-submit-page";
-import ResponsePage from "@/pages/response-page";
-import ResponseWritePage from "@/pages/response-write-page";
-import ResponseSubmitPage from "@/pages/response-submit-page";
+import QuestionPage from "@/pages/question/question-page";
+import QuestionSubmitPage from "@/pages/question/question-submit-page";
+import ResponsePage from "@/pages/response/response-page";
+import ResponseWritePage from "@/pages/response/response-write-page";
+import ResponseSubmitPage from "@/pages/response/response-submit-page";
+import LetterStorePage from "@/pages/letter/letter-store-page";
+import LetterReadPage from "@/pages/letter/letter-read-page";
+import LetterResponsePage from "@/pages/letter/letter-response-page";
 
 export default function App() {
   return (
@@ -17,6 +20,9 @@ export default function App() {
         <Route path="/response" element={<ResponsePage />} />
         <Route path="/response/write" element={<ResponseWritePage />} />
         <Route path="/response/submit" element={<ResponseSubmitPage />} />
+        <Route path="/letter/store" element={<LetterStorePage />} />
+        <Route path="/letter/read" element={<LetterReadPage />} />
+        <Route path="/letter/response" element={<LetterResponsePage />} />
       </Routes>
     </MainLayout>
   );
---
+++ src/pages/letter/letter-store-page.tsx
@@ -0,0 +1,42 @@
+import { useNavigate } from "react-router-dom";
+import Letter from "@/assets/letter.svg?react";
+
+const ResponseSubmitPage = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="w-full min-h-screen bg-gray-50 flex flex-col items-center justify-around font-suit p-5 gap-8">
+      <div className="w-full flex flex-col items-center justify-center gap-8">
+        {/* 상단 메시지 */}
+        <div>
+          <div className="w-full text-center text-2xl font-bold text-gray-700 leading-relaxed">
+            편지는 아카이브에 <br />
+            자동으로 저장되었어요!{" "}
+          </div>
+
+          <div className="text-center text-m font-medium text-gray-500 leading-relaxed">
+            원한다면 아카이브에서 삭제할 수 있어요{" "}
+          </div>
+        </div>
+        {/* 비행기 아이콘 */}
+        <div className="flex justify-center items-center">
+          <Letter className="w-50" />
+        </div>
+      </div>
+
+      <div className="w-full flex items-center justify-center gap-3">
+        <button className="w-full py-3 bg-black-400 text-white-100 rounded-lg hover:bg-blue-600 transition-colors">
+          아카이브 바로가기
+        </button>
+        <button
+          className="py-3 bg-black-500 text-white-100 rounded-lg hover:bg-blue-600 transition-colors w-1/2"
+          onClick={() => navigate("/")}
+        >
+          확인
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ResponseSubmitPage;
