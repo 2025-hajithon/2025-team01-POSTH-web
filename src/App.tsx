959679f3dae378e1d547af6a34a543ce919b0578
Lee JuHee <163210518+BeanMouse@users.noreply.github.com>
2025-07-26 20:33:14 +0900

    Feature: 답변 페이지 퍼블리싱 (#2)
    * feat: 페이지 퍼블리싱
    
    * feat: 질문 완료
    
    * feat: 답변 전달

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import MainLayout from "@/layouts/main-layout";
 import HomePage from "@/pages/home-page";
 import QuestionPage from "@/pages/question-page";
 import QuestionSubmitPage from "@/pages/question-submit-page";
+import ResponsePage from "@/pages/response-page";
+import ResponseWritePage from "@/pages/response-write-page";
+import ResponseSubmitPage from "@/pages/response-submit-page";
 
 export default function App() {
   return (
@@ -11,6 +14,9 @@ export default function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/question" element={<QuestionPage />} />
         <Route path="/question/submit" element={<QuestionSubmitPage />} />
+        <Route path="/response" element={<ResponsePage />} />
+        <Route path="/response/write" element={<ResponseWritePage />} />
+        <Route path="/response/submit" element={<ResponseSubmitPage />} />
       </Routes>
     </MainLayout>
   );
